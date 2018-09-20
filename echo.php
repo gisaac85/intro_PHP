d6769d32d04949a5d7ae6cd0114ef533d62c3187
Gaorieh Isaac <gaorieh@hotmail.com>
2018-09-20 23:56:27 +0200

    php files

---
+++ echo.php
@@ -0,0 +1,8 @@
+<?php
+$length=4;
+$square=4*$length;
+echo "Square= ".$square." [cm2]"."<br>";
+echo "Square= ".($square/10000)." [m2]"."<br>";
+echo "Square= ".($square/100000)." [mm2]"."<br>";
+echo chr(76);
+?>
